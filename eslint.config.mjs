ef9951d592ca28eab8247305a9792d6f936e8697
rasa <rasarais2002@gmail.com>
2026-01-12 16:12:39 -0500

    `Added "lint" script to package.json`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import js from '@eslint/js';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  tseslint.configs.recommended,
-]);
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  js.configs.recommended,
+];
