80b30d67c8b0c97c3c607417127601b3ab71940a
rasa <rasarais2002@gmail.com>
2026-01-27 13:52:44 -0500

    everything

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -1,37 +1,32 @@
-// src/auth.js
-// Passport Bearer auth using AWS Cognito ID tokens (JWT)
-
 const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
+const logger = require('../logger');
 
-const logger = require('./logger');
+if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
+  throw new Error('missing expected env vars: AWS_COGNITO_POOL_ID, AWS_COGNITO_CLIENT_ID');
+}
 
-// Create Cognito JWT verifier (expects ID token)
+logger.info('Using AWS Cognito for auth');
+
 const jwtVerifier = CognitoJwtVerifier.create({
   userPoolId: process.env.AWS_COGNITO_POOL_ID,
   clientId: process.env.AWS_COGNITO_CLIENT_ID,
   tokenUse: 'id',
 });
 
-logger.info('Configured to use AWS Cognito for Authorization');
-
-// Cache JWKS at startup
 jwtVerifier
   .hydrate()
-  .then(() => logger.info('Cognito JWKS successfully cached'))
+  .then(() => logger.info('Cognito JWKS cached'))
   .catch((err) => logger.error({ err }, 'Unable to cache Cognito JWKS'));
 
 module.exports.strategy = () =>
   new BearerStrategy(async (token, done) => {
     try {
       const user = await jwtVerifier.verify(token);
-      logger.debug({ user }, 'verified user token');
-
-      // Use email as the "user id" for now
       done(null, user.email);
     } catch (err) {
-      logger.error({ err }, 'could not verify token');
+      logger.error({ err, token }, 'could not verify token');
       done(null, false);
     }
   });
