f7541ab1fb47b41406c634cbf603be5b97369352
rasa <rasarais2002@gmail.com>
2026-03-23 18:22:37 -0400

    update lab 8

---
+++ scripts/local-aws-setup.sh
@@ -0,0 +1,35 @@
+
+echo "Setting AWS environment variables for LocalStack"
+
+echo "AWS_ACCESS_KEY_ID=test"
+export AWS_ACCESS_KEY_ID=test
+
+echo "AWS_SECRET_ACCESS_KEY=test"
+export AWS_SECRET_ACCESS_KEY=test
+
+export AWS_DEFAULT_REGION=us-east-2
+echo "AWS_DEFAULT_REGION=us-east-2"
+
+echo 'Waiting for LocalStack S3...'
+until (curl --silent http://localhost:4566/_localstack/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null); do
+    sleep 5
+done
+echo 'LocalStack S3 Ready'
+
+echo "Creating LocalStack S3 bucket: fragments"
+aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket fragments
+
+echo "Creating DynamoDB-Local DynamoDB table: fragments"
+aws --endpoint-url=http://localhost:8000 \
+dynamodb create-table \
+    --table-name fragments \
+    --attribute-definitions \
+        AttributeName=ownerId,AttributeType=S \
+        AttributeName=id,AttributeType=S \
+    --key-schema \
+        AttributeName=ownerId,KeyType=HASH \
+        AttributeName=id,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --endpoint-url=http://localhost:8000 dynamodb wait table-exists --table-name fragments
